ec45e61ebe58c4d123305ca8d5cbfb38f9e6b4eb
Mary Goodhart <mary.loves.pry@gmail.com>
2019-04-13 17:11:37 -0600

    Refactors test to accomodate scoring information

--- spec/features/user_can_see_search_form_spec.rb
+++ spec/features/user_can_see_search_form_spec.rb
@@ -7,12 +7,14 @@ describe 'when a user visits "/" ' do
     expect(page).to have_css("#location")
     expect(page).to have_button("Search")
   end
-  it "fills out the form with a city and state and gets a result" do
+  it "fills out the form with a city and state and gets results" do
     visit '/'
     city_state = "Denver, Co"
     population = 2888227
     income = 48435
     walkscore = 93
+    bikescore = 94
+    transitscore = "unavailable for this city"
     fill_in :location, with: city_state
     click_button "Search"
     expect(current_path).to eq(search_path)
@@ -21,6 +23,8 @@ describe 'when a user visits "/" ' do
       expect(page).to have_content("The population of #{city_state} is #{population}.")
       expect(page).to have_content("The per-capita-income of #{city_state} is #{income}.")
       expect(page).to have_content("The walk score for #{city_state} is #{walkscore}.")
+      expect(page).to have_content("The bike score for #{city_state} is #{transitscore}.")
+      expect(page).to have_content("The transit score for #{city_state} is #{bikescore}.")
     end
   end
 end
