123ce77d4067e083f2f9e67701c81c3e6493b400
Mary Goodhart <mary.loves.pry@gmail.com>
2019-04-13 12:50:40 -0600

    Changes ODS test and service to reflect properly searching for population

--- app/services/ods_service.rb
+++ app/services/ods_service.rb
@@ -1,7 +1,11 @@
 class ODSService
+  def initialize(location)
+    @location = location
+  end
 
-  def returned_info
-    JSON.parse(response.body, symbolize_names: true)
+  def population_info
+    response = conn.get("/api/records/1.0/search/?dataset=us-population-urban-area&q=#{@location}&rows=1&sort=year")
+    JSON.parse(response.body, symbolize_names: true)[:records][0]
   end
 
   def conn
@@ -10,7 +14,11 @@ class ODSService
     end
   end
 
-  def response
-    conn.get("/api/records/1.0/search/?dataset=us-population-urban-area&sort=year&facet=geography&facet=year")
-  end
+  # def response
+  #   conn.get("/api/records/1.0/search/?dataset=us-population-urban-area&sort=year&facet=geography&facet=year")
+  # end
+  #
+  # def population_response(location)
+  #   "/api/records/1.0/search/?dataset=us-population-urban-area&q=Austin%2C+TX&rows=1&sort=year"
+  # end
 end
--- spec/services/opendatasoft_service_spec.rb
+++ spec/services/opendatasoft_service_spec.rb
@@ -1,19 +1,23 @@
 require 'rails_helper'
 
 describe ODSService do
+  before(:each) do
+    @location = "Denver, CO"
+  end
   it 'exists' do
-    ods = ODSService.new
+    ods = ODSService.new(@location)
 
     expect(ods).to be_a(ODSService)
   end
   it 'returns data for something' do
-    ods = ODSService.new
+    ods = ODSService.new(@location)
 
-    expect(ods.returned_info).to be_a(Hash)
+    expect(ods.population_info).to be_a(Hash)
   end
   it 'returns data for population of a particular city' do
-    ods = ODSService.new
+    ods = ODSService.new(@location)
 
-    expect(ods.returned_info).to be_a(Hash)
+    expect(ods.population_info).to be_a(Hash)
+    expect(ods.population_info[:fields]).to have_key(:population)
   end
 end
