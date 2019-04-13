7e5080cb10fd637c67feb1cb3187a49265b2c8a0
Mary Goodhart <mary.loves.pry@gmail.com>
2019-04-13 13:56:05 -0600

    Adds income_info method

--- app/services/ods_service.rb
+++ app/services/ods_service.rb
@@ -8,17 +8,16 @@ class ODSService
     JSON.parse(response.body, symbolize_names: true)[:records][0]
   end
 
+  def income_info
+    response = conn.get("/api/records/1.0/search/?dataset=real-per-capita-income-by-metropolitan-statistical-area&q=#{@location}&rows=1")
+    JSON.parse(response.body, symbolize_names: true)[:records][0]
+  end
+
   def conn
     Faraday.new("https://public.opendatasoft.com") do |f|
       f.adapter Faraday.default_adapter
     end
   end
 
-  # def response
-  #   conn.get("/api/records/1.0/search/?dataset=us-population-urban-area&sort=year&facet=geography&facet=year")
-  # end
-  #
-  # def population_response(location)
-  #   "/api/records/1.0/search/?dataset=us-population-urban-area&q=Austin%2C+TX&rows=1&sort=year"
-  # end
+
 end
