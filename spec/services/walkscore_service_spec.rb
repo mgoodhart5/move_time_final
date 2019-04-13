005c371eef0d45e654ae317a56414c3856f73310
Mary Goodhart <mary.loves.pry@gmail.com>
2019-04-13 16:36:16 -0600

    Adds to walkscore test

--- spec/services/walkscore_service_spec.rb
+++ spec/services/walkscore_service_spec.rb
@@ -6,5 +6,15 @@ describe WalkscoreService do
 
     expect(wss).to be_a(WalkscoreService)
   end
+  it 'returns the walk, bike, transit scores' do
+    location = "Denver, CO"
+    geo_s = GeoService.new(location)
+    wss = WalkscoreService.new
+    results = wss.scores(geo_s.latitude, geo_s.longitude)
 
+    expect(results).to be_a(Hash)
+    expect(results).to have_key(:walkscore)
+    expect(results[:transit]).to have_key(:score)
+    expect(results[:bike]).to have_key(:score)
+  end
 end
