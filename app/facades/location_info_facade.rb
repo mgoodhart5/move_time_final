b1107d0f6a7f5dbdc4d9160b9f0815389a85d602
Mary Goodhart <mary.loves.pry@gmail.com>
2019-04-13 15:45:54 -0600

    Adds income to facade

--- app/facades/location_info_facade.rb
+++ app/facades/location_info_facade.rb
@@ -1,14 +1,21 @@
 class LocationInfoFacade
   attr_reader :location,
-              :population
-              
+              :population,
+              :income
+
   def initialize(location)
     @location = location
     @population = population
+    @income = income
   end
 
   def population
     ods = ODSService.new(@location)
     ods.population_info[:fields][:population]
+  end
+
+  def income
+    ods = ODSService.new(@location)
+    ods.income_info[:fields][:"2015"]
   end
 end
