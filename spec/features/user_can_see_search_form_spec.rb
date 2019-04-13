5fcc57d98ce692c42bce9ff60781def27cc9bac4
Mary Goodhart <mary.loves.pry@gmail.com>
2019-04-13 16:36:32 -0600

    Refactors feature test to include walkscore

--- spec/features/user_can_see_search_form_spec.rb
+++ spec/features/user_can_see_search_form_spec.rb
@@ -12,13 +12,15 @@ describe 'when a user visits "/" ' do
     city_state = "Denver, Co"
     population = 2888227
     income = 48435
+    walkscore = 93
     fill_in :location, with: city_state
     click_button "Search"
     expect(current_path).to eq(search_path)
 
     within "#all-info" do
-      expect(page).to have_content("The population of #{city_state} is #{population}")
-      expect(page).to have_content("The per-capita-income of #{city_state} is #{income}")
+      expect(page).to have_content("The population of #{city_state} is #{population}.")
+      expect(page).to have_content("The per-capita-income of #{city_state} is #{income}.")
+      expect(page).to have_content("The walk score for #{city_state} is #{walkscore}.")
     end
   end
 end
