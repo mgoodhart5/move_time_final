759a06bed92c164132b1c391b3f7e3e247e2ba96
Mary Goodhart <mary.loves.pry@gmail.com>
2019-04-13 15:45:34 -0600

    Adds to features test to account for population information

--- spec/features/user_can_see_search_form_spec.rb
+++ spec/features/user_can_see_search_form_spec.rb
@@ -11,12 +11,14 @@ describe 'when a user visits "/" ' do
     visit '/'
     city_state = "Denver, Co"
     population = 2888227
+    income = 48435
     fill_in :location, with: city_state
     click_button "Search"
     expect(current_path).to eq(search_path)
 
-    within "#population-info" do
+    within "#all-info" do
       expect(page).to have_content("The population of #{city_state} is #{population}")
+      expect(page).to have_content("The per-capita-income of #{city_state} is #{income}")
     end
   end
 end
