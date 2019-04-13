7d6183afb68e5609e016a36ff8d0fc7aa3e65bbc
Mary Goodhart <mary.loves.pry@gmail.com>
2019-04-13 16:37:02 -0600

    Adds walkscore to location facade

--- app/facades/location_info_facade.rb
+++ app/facades/location_info_facade.rb
@@ -18,4 +18,10 @@ class LocationInfoFacade
     ods = ODSService.new(@location)
     ods.income_info[:fields][:"2015"]
   end
+
+  def walkscore
+    geo_service = GeoService.new(@location)
+    walkscore_service = WalkscoreService.new
+    walkscore_service.scores(geo_service.latitude, geo_service.longitude)[:walkscore]
+  end
 end
