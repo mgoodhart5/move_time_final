3eb0fab97c4e3cd9cfb82b4e7ebefd16477d7b0d
Mary Goodhart <mary.loves.pry@gmail.com>
2019-04-14 12:16:12 -0600

    Adds relationships to spec

--- spec/models/favorite_spec.rb
+++ spec/models/favorite_spec.rb
@@ -4,4 +4,7 @@ describe Favorite do
   describe 'Validations' do
     it { should validate_presence_of(:location_name) }
   end
+  describe 'Relationships' do
+    it { should have_many(:users).through(:user_favorites)}
+  end
 end
