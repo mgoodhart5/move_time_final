0466fd4044b27d88d343d9c8f2f5d7e4e56bd3bd
Mary Goodhart <mary.loves.pry@gmail.com>
2019-04-15 14:07:16 -0600

    Adds seeds and adds favorites route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root "welcome#index"
   get "/search", to: "search#index"
+  post "/users/:id/favorites", to: "favorites#create", as: "favorite"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
